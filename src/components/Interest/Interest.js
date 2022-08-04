525824b021d3adeb43c88904d8b399a723b62a08
rupen11 <jarsaniyarupen7984@gmail.com>
2022-08-04 07:52:57 +0530

    preferences funcnality

--- src/components/Interest/Interest.js
+++ src/components/Interest/Interest.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Interest.css'
 import Plus from '../../assets/icons/plus.svg'
 import Right from '../../assets/icons/right.svg'
 
-const Interest = ({ name, svg, status }) => {
+const Interest = ({ name, svg }) => {
 
-    const selectInterest = (name, status) => {
-        // const intrest = prefrences_arr.filter((item) => item.interest_name === name)
-        status = 'select'
-    }
+    const [status, setStatus] = useState('none');
+
+    const selectInterest = status => status === 'none' ? setStatus('select') : setStatus('none');
 
     return (
-        <div className={`interest_card ${status === 'select' && 'interest_select'}`} onClick={() => selectInterest(name, status)}>
+        <div className={`interest_card ${status === 'select' && 'interest_select'}`} onClick={() => selectInterest(status)}>
             <img src={svg} alt="" />
             <p className="interest_name">{name}</p>
             <img src={status === 'none' ? Plus : Right} alt="" />
